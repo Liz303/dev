686b37ff66b2664c08aeb839cc4a70a3e0b51d60
fbushell <frank@gocirro.com>
2012-08-02 20:50:33 -0600

    added sass and css folders

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <!doctype html>
 <head>
   <meta charset="utf-8">
+  <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
+
 <?php 
 
-	include "blog_posts.inc";
+	//include "blog_posts.inc";
 	
-	print_r($blog_posts);
+	//print_r($blog_posts);
 
 ?>
+
+<div class="container">
+	<div class="inner">
+		<h3>Liz was here</h3>
+	</div>
+</div>
+
 
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
