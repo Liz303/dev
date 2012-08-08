642bd39dcfdd6647cc30a5ff32b08fea6c0efd75
Liz303 <liz@cirrously.com>
2012-08-07 21:49:49 -0600

    messing around with php

---
+++ phpractice.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<h1> Hello </h1>
+
+<?php
+$username = "Liz";
+$ima = "sweet ass bitch";
+
+print 'Liz is a '. $ima;
+
+
+?>
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+</body>
+</html>
