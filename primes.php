f7c1b18fadaf63dd39dee536ca7f0ba058916acb
Liz303 <liz@cirrously.com>
2012-08-13 21:49:26 -0600

    php for prime numbers

--- primes.php
+++ primes.php
@@ -1,9 +1,21 @@
-<?php
 
-// Loop through numbers from 0 to 1000
-for ($i = 0; $i <= 1000; $i++) {
+<?php 
 
-	print $i;
-	print "<br />";
-	
-}
+//Loop through numbers 2 to 1000
+$start = microtime();
+	for($i=2;$i<=1000;$i++) { 
+		$prime=0;
+		for($j=2;$j<=$i/2;$j++) 
+//Check if the remainder is zero
+		if(($i%$j)==0) { 
+			$prime=1;  
+		} 
+		if($prime==0) 
+		print "$i <br />";
+	}
+print "<br />";
+$end = microtime();
+$totalTime = $end-$start;
+
+print "Took $totalTime seconds.";
+			
